48ee9ae4910454aebad2ed6eb287a5b7e327dc84
virtualis9 <peacockchristopher15@gmail.com>
2024-11-25 15:42:32 +0000

    pushed to pull

--- resources/js/Pages/Movies/Index.vue
+++ resources/js/Pages/Movies/Index.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { Link, router } from '@inertiajs/vue3';
 import { ref, watch, defineEmits } from 'vue';
@@ -7,15 +6,6 @@ import { debounce } from 'underscore';
 import FeaturedMovie from '@/Movie/FeaturedMovie.vue';
 import MovieList from '@/Movie/MovieList.vue';
 import Paginator from '@/Widgets/Paginator.vue';
-=======
-import GuestLayout from "@/Layouts/GuestLayout.vue";
-import { Link, router } from "@inertiajs/vue3";
-import { ref, computed, watch } from "vue";
-import { debounce } from "underscore";
-import FeaturedMovie from "@/Movie/FeaturedMovie.vue";
-import MovieList from "@/Movie/MovieList.vue";
-import Paginator from "@/Widgets/Paginator.vue";
->>>>>>> 1d1fded975ee1df02d6a57ab14eb33c52c9a675b
 
 const emit = defineEmits({
 //
@@ -33,7 +23,6 @@ const props = defineProps({
     },
     featuredMovie: {
         required: false,
-<<<<<<< HEAD
         type: Object
     },
     genres: {
@@ -44,23 +33,12 @@ const props = defineProps({
 
 // Define search input and selected genre as reactive refs
 let { searchInput, featuredMovie, genres } = props;
-=======
-        type: Object,
-    },
-});
-
-let { searchInput, featuredMovie } = props;
->>>>>>> 1d1fded975ee1df02d6a57ab14eb33c52c9a675b
 const search = ref(searchInput);
 const selectedGenre = ref('');  // This will store the selected genre
 
 // Movie search logic
 function movieMatchesSearch(movie, str) {
-<<<<<<< HEAD
     return movie.name.toLowerCase().includes(str.toLowerCase());  // Case-insensitive search
-=======
-    return movie.name.indexOf(str) !== -1;
->>>>>>> 1d1fded975ee1df02d6a57ab14eb33c52c9a675b
 }
 
 // Method to trigger search
@@ -68,7 +46,6 @@ function doSearch() {
     router.reload({
         data: {
             search: search.value,
-<<<<<<< HEAD
             genre: selectedGenre.value,  // Include genre in the search parameters
             page: 1  // Reset to page 1 when a new search is made
         },
@@ -82,25 +59,10 @@ const searchDebouncer = debounce(doSearch, 800);
 // Watch search input and genre to trigger the debounced search
 watch([search, selectedGenre], searchDebouncer);  // Watch both search and genre to trigger the search when either changes
 
-=======
-            page: 1,
-        },
-        only: ["paginator"],
-    });
-}
-
-function generateRandomMovie() {
-    router.visit(route("movies.random")); // Redirect to the random movie
-}
-
-const searchDebouncer = debounce(doSearch, 800);
-
-watch(search, searchDebouncer);
->>>>>>> 1d1fded975ee1df02d6a57ab14eb33c52c9a675b
 </script>
+
 <template>
     <div class="container mx-auto">
-<<<<<<< HEAD
 
         <!-- Featured Movie -->
         <FeaturedMovie v-if="featuredMovie" :movie="featuredMovie" />
@@ -112,18 +74,6 @@ watch(search, searchDebouncer);
                 v-model="search" 
                 placeholder="Search for your favourite movie"
                 class="input input-bordered w-full max-w-xs mr-5">
-=======
-        <FeaturedMovie v-if="featuredMovie" :movie="featuredMovie" />
-
-        <form class="my-5 flex items-center" @submit.prevent="doSearch">
-            <input
-                type="text"
-                v-model="search"
-                placeholder="Search for your favourite movie"
-                class="input input-bordered w-full max-w-xs mr-5"
-            />
-
->>>>>>> 1d1fded975ee1df02d6a57ab14eb33c52c9a675b
             <button class="btn btn-secondary">Search</button>
             <button
                 class="btn btn-success"
