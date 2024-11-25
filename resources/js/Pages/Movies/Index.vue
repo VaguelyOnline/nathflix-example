c964110fdbe66a43bfe79fbb0dbc63b93fec25ef
Tomas <TomasHRichards@outlook.com>
2024-11-25 14:45:13 +0000

    wip

--- resources/js/Pages/Movies/Index.vue
+++ resources/js/Pages/Movies/Index.vue
@@ -1,77 +1,75 @@
 <script setup>
-
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import { Link, router } from '@inertiajs/vue3';
-import { ref, computed, watch } from 'vue';
-import { debounce } from 'underscore';
-import FeaturedMovie from '@/Movie/FeaturedMovie.vue';
-import MovieList from '@/Movie/MovieList.vue';
-import Paginator from '@/Widgets/Paginator.vue';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Link, router } from "@inertiajs/vue3";
+import { ref, computed, watch } from "vue";
+import { debounce } from "underscore";
+import FeaturedMovie from "@/Movie/FeaturedMovie.vue";
+import MovieList from "@/Movie/MovieList.vue";
+import Paginator from "@/Widgets/Paginator.vue";
 
 const props = defineProps({
     paginator: {
         required: true,
-        type: Object
+        type: Object,
     },
     searchInput: {
         required: false,
         type: String,
-        default: ''
+        default: "",
     },
     featuredMovie: {
         required: false,
-        type: Object
-    }
+        type: Object,
+    },
 });
 
 let { searchInput, featuredMovie } = props;
-// const paginator = ref(props.paginator);
 const search = ref(searchInput);
 
 function movieMatchesSearch(movie, str) {
-    return movie.name.indexOf(str) !== -1
+    return movie.name.indexOf(str) !== -1;
 }
 
 function doSearch() {
     router.reload({
         data: {
             search: search.value,
-            page: 1
+            page: 1,
         },
-        only: [
-            'paginator',
-        ]
+        only: ["paginator"],
     });
 }
 
-// Create a function that will trigger doSearch when it hasn't been invoked for 800ms
+function generateRandomMovie() {
+    const randomId = Math.floor(Math.random() * 9000) + 1; // Generates a number between 1 and 9000
+    window.location.href = `/movies/${randomId}`; // Redirect to the random movie
+}
+
 const searchDebouncer = debounce(doSearch, 800);
 
-// When the search ref changes - call the debouncer that will trigger the search
 watch(search, searchDebouncer);
-
 </script>
 
 <template>
-
     <div class="container mx-auto">
-
         <FeaturedMovie v-if="featuredMovie" :movie="featuredMovie" />
-        
+
         <form class="my-5 flex items-center" @submit.prevent="doSearch">
-            <input 
-                type="text" 
+            <input
+                type="text"
                 v-model="search"
-                placeholder="Search for your favourite movie" 
-                class="input input-bordered w-full max-w-xs mr-5">
+                placeholder="Search for your favourite movie"
+                class="input input-bordered w-full max-w-xs mr-5"
+            />
 
             <button class="btn btn-secondary">Search</button>
+            <button class="btn btn-success" @click="generateRandomMovie">
+                Generate Random Movie
+            </button>
         </form>
 
         <Paginator class="mb-5" :links="props.paginator.links" />
 
         <MovieList :movies="props.paginator.data" />
-
     </div>
-
 </template>
